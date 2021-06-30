f36cfc37ff590eaf89a0e51fef3e51c269de5883
Francyelle <francyellem64@gmai.com>
2021-06-30 17:46:59 -0300

    final config tsx

--- reactJs/chapter_1/src/components/RepositoryList.jsx
+++ reactJs/chapter_1/src/components/RepositoryList.jsx
@@ -14,7 +14,7 @@ export function RepositoryList() {
     
     return (
         <section className="repository-list">
-        <h1>Lista de repositórios</h1>
+        <h1>Lista de repositórios da fran</h1>
 
         <ul>
             {repositories.map(repository => {
