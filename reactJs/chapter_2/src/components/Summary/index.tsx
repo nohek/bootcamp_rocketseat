c3ccc62ea2617a0aa0f0d96d0f71d094a656fbcd
Francyelle <francyellem64@gmai.com>
2021-07-02 17:01:43 -0300

    Chapter2 - Modal & Forms

---
+++ reactJs/chapter_2/src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import incomesImg from '../../assets/incomes.svg'
+import expensesImg from '../../assets/expenses.svg'
+import totalImg from '../../assets/total.svg'
+
+import {Container} from './styles'
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomesImg} alt="Entradas" />
+        </header>
+        <strong> R$ 1.000,00 </strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={expensesImg} alt="Saidas" />
+        </header>
+        <strong> - R$ 500,00 </strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong> R$ 500,00 </strong>
+      </div>
+    </Container>
+  )
+}
