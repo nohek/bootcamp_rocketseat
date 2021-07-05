32fbfaa687a787c8ec201e380ef4dddf386486bf
Francyelle <francyellem64@gmai.com>
2021-07-05 16:45:29 -0300

    Finalizando inserção

--- reactJs/chapter_2/src/TransactionsContext.tsx
+++ reactJs/chapter_2/src/TransactionsContext.tsx
@@ -16,7 +16,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
@@ -33,8 +33,17 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
     .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('./transactions', transaction)
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('./transactions', {
+      ...transactionInput,
+      createdAt: new Date()
+     }) 
+    const {transaction} = response.data
+
+    setTransactions([
+      ...transactions,
+      transaction,
+    ])
   }
 
   return (
